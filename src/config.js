35366e64801aa18516690dd0739ce1626333ebaf
Gabriel Vautrin <gvautrin@serial.ch>
2020-03-03 09:22:58 +0100

    feat: aws config

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+    s3: {
+      REGION: "eu-west-3",
+      BUCKET: "serial-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "eu-west-3",
+      URL: "https://htyhkikgc0.execute-api.eu-west-3.amazonaws.com/prod/"
+    },
+    cognito: {
+      REGION: "eu-west-1",
+      USER_POOL_ID: "eu-west-1_B7uzJuZYp",
+      APP_CLIENT_ID: "2khdrvsm3j616bkmd3p4gab96a",
+      IDENTITY_POOL_ID: "eu-west-1:c32008db-e295-48fc-b497-a4015d2a5feb"
+    }
+  };
+  
