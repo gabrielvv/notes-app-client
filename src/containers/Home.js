971185f1df8aa47d21833d164aa370d0f66d674d
Gabriel Vautrin <gvautrin@serial.ch>
2020-03-04 09:44:33 +0100

    Updating the lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { API } from 'aws-amplify';
 import { LinkContainer } from 'react-router-bootstrap';
@@ -12,24 +13,24 @@ export default function Home(props) {
       if (!props.isAuthenticated) {
         return;
       }
-  
+
       try {
         const notes = await loadNotes();
         setNotes(notes);
       } catch (e) {
         alert(e);
       }
-  
+
       setIsLoading(false);
     }
-  
+
     onLoad();
   }, [props.isAuthenticated]);
-  
+
   function loadNotes() {
     return API.get("notes", "/notes");
   }
-  
+
   function renderNotesList(notes) {
     return [{}].concat(notes).map((note, i) =>
       i !== 0 ? (
@@ -39,14 +40,14 @@ export default function Home(props) {
           </ListGroupItem>
         </LinkContainer>
       ) : (
-        <LinkContainer key="new" to="/notes/new">
-          <ListGroupItem>
-            <h4>
-              <b>{"\uFF0B"}</b> Create a new note
+          <LinkContainer key="new" to="/notes/new">
+            <ListGroupItem>
+              <h4>
+                <b>{"\uFF0B"}</b> Create a new note
             </h4>
-          </ListGroupItem>
-        </LinkContainer>
-      )
+            </ListGroupItem>
+          </LinkContainer>
+        )
     );
   }
 
@@ -55,6 +56,14 @@ export default function Home(props) {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
